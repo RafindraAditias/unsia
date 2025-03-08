563a5b9486581cb8a0e07d34f31cb9ca7de69c4b
RafindraAditias <rafindra.aditias@gmail.com>
2025-03-08 15:41:42 +0700

    grapik

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+    <div>
+        <div className='rounded-md bg-gradient-to-r from-[#838FB9] to-[#3EC9D6]'>
+            Selamat Pagi, <br /> Alberd Flores
+        </div>
+        <div>
+
+        </div>
+    </div>
+
+    <div>
+
+    </div>
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import NavbarHome from "./components/NavbarHome";
 import UniversityDashboard from "./components/UniversityDashboard";
 import Footer from "./components/Footer";
+import Sidebar from "./components/dashboard/SideBar";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="pt-[125px] px-[25px] bg-[#fafafa] text-black">
         <UniversityDashboard />
       </div>
+      <Sidebar />
       <Footer />
     </div>
   );
